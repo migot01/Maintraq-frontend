4159b043e7f9101b42ba8eb39378807fb06f1d24
patrick migot <ptrckmigot@gmail.com>
2018-06-21 06:13:08 +0300

    [feature #158508111] Add signup page

---
+++ js/register.js
@@ -0,0 +1,23 @@
+// Add event listener
+document.getElementById('register').addEventListener('click', register);
+
+// register a user
+function registeruser(e) {
+	e.preventDefault();
+
+    let first_name= document.getElementById('first_name').value;
+    let last_name= document.getElementById('last_name').value;
+	let email = document.getElementById('email').value;
+	let password = document.getElementById('password').value;
+
+	fetch ('http://127.0.0.1:5000/api/v2/auth/register', {
+		method: 'POST',
+		headers: {
+			"Accept":"application/json",
+			"Content-type":"application/json"
+		},
+		body: JSON.stringify({first_name:fast_name,last_name:last_name, email:email, password:password})
+	})	
+	.then((res) => res.json())
+	.then((data) => console.log(data))
+}
