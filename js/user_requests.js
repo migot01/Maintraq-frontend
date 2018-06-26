2739f0f0bd4a8df0aa2dd21b3760d8ccf0fca3a1
patrick migot <ptrckmigot@gmail.com>
2018-06-26 11:25:57 +0300

    [Feature 158605473] Add user can view specific request

--- js/user_requests.js
+++ js/user_requests.js
@@ -27,15 +27,37 @@ function UserRequestsList() {
 					
 				
 				
-					id.innerHTML = request.id;
+					id.innerHTML ="<a href='user_view_a_request.html '>"+request.id+"</a>";
 					title.innerHTML = request.title;
 					location.innerHTML = request.location;
 					body.innerHTML = request.body;
 					status.innerHTML = request.status;
 					
-					i++;	
+					i++;
+					id.addEventListener('click', viewRequest);	
 				
 			} 
+
+			function viewRequest() {
+				fetch('http://127.0.0.1:5000/api/v2/users/requests/'+(request.id), {
+					method: 'GET',
+					headers:{
+						"Accept":"application/json",
+						"Content-type":"application/json",
+						"access-token":localStorage.getItem('auth-token')
+					}
+				})
+				.then((res) => res.json())
+				.then((data) => {
+					/*console.log(data.id);*/
+					localStorage.setItem('id', data.id);
+					localStorage.setItem('title', data.title);
+					localStorage.setItem('location', data.location);
+					localStorage.setItem('body', data.body);
+					localStorage.setItem('status', data.status);
+					location.assign('user_view_a_request.html');
+				})
+			}
 			
 			
 
